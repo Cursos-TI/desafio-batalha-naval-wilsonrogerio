69b3b25f96fd61c66390c4a083ce0d9c6ac6e859
wilsonrogerio <108371734+wilsonrogerio@users.noreply.github.com>
2025-03-01 20:22:08 +0000

    corrigido verificacao da posicao dos navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,26 +6,30 @@ void main() {
     int linha[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-    // Inicializa o tabuleiro
+    // Inicializar o tabuleiro
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
             tabuleiro[i][j] = 0;
         }
     }
 
-    // Adiciona navio horizontal
+    // Adicionar navio horizontal com verificação dentro do laço
     for (j = 6; j < 9; j++) {
-        if (tabuleiro[2][j] == 0) {
+        if (j < 10 && tabuleiro[2][j] == 0) {
             tabuleiro[2][j] = 3;
+        } else if (j >= 10) {
+            printf("Posição (%d, %c) está fora dos limites! Navio horizontal não pode ser colocado.\n", 2, coluna[j]);
         } else {
             printf("Posição (%d, %c) já ocupada! Navio horizontal não pode ser colocado.\n", 2, coluna[j]);
         }
     }
 
-    // Adiciona navio vertical
+    // Adicionar navio vertical com verificação dentro do laço
     for (i = 5; i < 8; i++) {
-        if (tabuleiro[i][6] == 0) {
+        if (i < 10 && tabuleiro[i][6] == 0) {
             tabuleiro[i][6] = 3;
+        } else if (i >= 10) {
+            printf("Posição (%d, %c) está fora dos limites! Navio vertical não pode ser colocado.\n", i, coluna[6]);
         } else {
             printf("Posição (%d, %c) já ocupada! Navio vertical não pode ser colocado.\n", i, coluna[6]);
         }
