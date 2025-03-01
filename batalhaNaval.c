da71a530ee4b67f55bedeec083e32d6c89b86122
wilsonrogerio <108371734+wilsonrogerio@users.noreply.github.com>
2025-03-01 20:12:36 +0000

    adiconado todas as exigencias nivel novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
-int main() {
+
+void main() {
     int tabuleiro[10][10];
     int i, j;
     int linha[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-    // Exibir as letras das colunas
+    // Inicializa o tabuleiro
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+    // Adiciona navio horizontal
+    for (j = 6; j < 9; j++) {
+        if (tabuleiro[2][j] == 0) {
+            tabuleiro[2][j] = 3;
+        } else {
+            printf("Posição (%d, %c) já ocupada! Navio horizontal não pode ser colocado.\n", 2, coluna[j]);
+        }
+    }
+
+    // Adiciona navio vertical
+    for (i = 5; i < 8; i++) {
+        if (tabuleiro[i][6] == 0) {
+            tabuleiro[i][6] = 3;
+        } else {
+            printf("Posição (%d, %c) já ocupada! Navio vertical não pode ser colocado.\n", i, coluna[6]);
+        }
+    }
+
+    // Letras das colunas
     printf("  ");
     for (j = 0; j < 10; j++) {
         printf("%c ", coluna[j]);
@@ -15,12 +41,9 @@ int main() {
     for (i = 0; i < 10; i++) {
         printf("%d ", linha[i]);
         for (j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
-
-    return 0;
 }
 
